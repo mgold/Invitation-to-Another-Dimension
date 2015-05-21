3722ef79b1db219034d5e12185b20c2f94c374e0
Max Goldstein <maxgoldstein1@gmail.com>
2015-05-20 22:33:51 -0400

    Improve code for progressive disclosure

--- matrix.js
+++ matrix.js
@@ -19,12 +19,10 @@ var y = d3.scale.linear()
         .domain([-5, 5])
         .range([200, -200])
 
-var symbols = svg.append("g")
+svg.append("g")
     .translate(100, 250)
     .attr("id", "symbols")
 
-symbols.append("text").text("a = mx + b")
-var dynamic_symbols = symbols.append("text").translate(0, 30)
 
 var plot = svg.append("g")
         .translate(600, 250)
@@ -39,70 +37,100 @@ var data = d3.range(-3, 4)
 
 function render(m, b, initialRender){
     var f = function(x){return m*x + b}
+    var transDur = 1000;
 
-    dynamic_symbols.text("a = " + m.toFixed(2) + "x + " + b.toFixed(2))
+    var symbols = d3.select("#symbols").selectAll("text")
+        .data(["a = mx + b", "a = " + m.toFixed(2) + "x + " + b.toFixed(2)])
+    symbols.enter().append("text")
+        .style("opacity", 0)
+      .transition().duration(500).delay(transDur*3)
+        .style("opacity", 1)
+    symbols.exit()
+      .transition().duration(500)
+        .style("opacity", 0)
+        .remove();
+    symbols.text(function(d){return d})
+        .translate(function(d,i){return [0, 30*i]})
+
 
     var circles = plot.selectAll("g.datapoint")
         .data(data)
     circles.exit().transition().attr("r", 0).remove();
-    renderDatapoint = function(sel){
-        sel.select("circle.x")
-            .attr("r", 4)
-            .attr("cx", function(d){ return x(d)})
-            .attr("cy", 0)
-        sel.select("line.a")
-            .attr("x1", function(d){ return x(d)})
-            .attr("x2", function(d){ return x(d)})
-            .attr("y2", function(d){ return -x(f(d))})
+
+    renderCircleX = function(sel){
+        sel.attr("r", 4)
+           .attr("cx", function(d){ return x(d)})
     }
-    renderDatapoint(circles); // must be before appending to enter selection
+    renderLineA = function(sel){
+        sel.attr("y2", function(d){ return -x(f(d))})
+    }
+    renderCircleA = function(sel){
+        sel.attr("r", function(d){return d ? 4 : 3})
+           .attr("cy", function(d){ return -x(f(d))})
+    }
+
+    // update selection only - must be called before appending to enter
+    circles.select("circle.x").call(renderCircleX)
+    circles.select("line.a").call(renderLineA)
+    circles.select("circle.a").call(renderCircleA)
+
     var entering = circles.enter().append("g").attr("class", "datapoint")
+
     entering.append("circle")
         .attr("class", "x")
         .attr("r", 0)
         .attr("cx", x(0))
         .attr("cy", 0)
+      .transition().duration(transDur)
+        .call(renderCircleX)
+
     entering.append("line")
         .attr("class", "a")
-        .attr({x1: x(0), x2: x(0), y1: 0, y2: 0})
+        .attr("x1", function(d){ return x(d)})
+        .attr("x2", function(d){ return x(d)})
+        .attr({y1: 0, y2: 0})
+      .transition().delay(transDur).duration(transDur)
+        .call(renderLineA)
 
-    entering = entering.transition().duration(1000).call(renderDatapoint);
+    entering.append("circle")
+        .attr("r", 0)
+        .attr("cx", function(d){ return x(d)})
+        .attr("cy", 0)
+        .attr("class", "a")
+      .transition().delay(transDur).duration(transDur)
+        .call(renderCircleA)
 
     var dx = x.range()[0]
     var dy = f(x(-3))
     var len = Math.sqrt(dx*dx + dy*dy)
+    var rot = Math.atan(m) * -180 / Math.PI;
     if (initialRender){
-        entering.each("end", function (d, i){
-            if (i) return;
-            var drag = d3.behavior.drag()
-                    .on("drag", function(evt){
-                        if (d3.event.x == 0){
-                            render(9999.99, b)
-                        }else{
-                            render(-d3.event.y/d3.event.x, b);
-                        }
-                    })
-            var rot = Math.atan(m) * -180 / Math.PI;
-            var dragHandle = plot.append("g")
-                .attr("id", "dragger")
-                .attr("transform", "rotate("+rot+") translate(0,"+b+")")
-                .call(drag)
-            dragHandle.append("line")
-                .attr({x1: -len, x2: -len, y1: 0, y2: 0})
-                .attr("class", "a")
-                .transition()
-                .duration(1000)
-                .attr({x2: len})
-            dragHandle.append("line")
-                .attr("class", "cover")
-                .attr({x1: -len, x2: len, y1: 0, y2: 0})
-        })
+        var drag = d3.behavior.drag()
+            .on("drag", function(evt){
+                if (d3.event.x == 0){
+                    render(9999.99, b)
+                }else{
+                    render(-d3.event.y/d3.event.x, b);
+                }
+            })
+        var dragHandle = plot.append("g")
+            .attr("id", "dragger")
+            .attr("transform", "rotate("+rot+") translate(0,"+b+")")
+            .call(drag)
+        dragHandle.append("line")
+            .attr({x1: -len, x2: -len, y1: 0, y2: 0})
+            .attr("class", "a")
+          .transition().duration(transDur).delay(2*transDur)
+            .attr({x2: len})
+        dragHandle.append("line")
+            .attr("class", "cover")
+            .attr({x1: -len, x2: len, y1: 0, y2: 0})
     }else{
-        var rot = Math.atan(m) * -180 / Math.PI;
         d3.select("#dragger")
             .attr("transform", "rotate("+rot+") translate(0,"+b+")")
           .select("line")
             .attr({x1: -len, x2: len, y1: 0, y2: 0})
     }
 }
+
 render(1, 0, true);
