aecb04ec6039aaeeda1ef00ec07f24106ff53f59
Max Goldstein <maxgoldstein1@gmail.com>
2015-05-23 15:49:35 -0400

    Refactor to split apart large function

--- matrix.js
+++ matrix.js
@@ -1,3 +1,5 @@
+// Freezing ia a simple counting semaphore to prevent changing the stage during
+// a transition.
 var _freeze = 0
 function freeze(){
     _freeze++;
@@ -5,6 +7,9 @@ function freeze(){
 function unfreeze(){
     if (_freeze <= 0) console.warn("Unfreezing when freeze is", _freeze)
     _freeze--;
+}
+function isFrozen(){
+    return _freeze > 0;
 }
 
 var svg = d3.select("svg")
@@ -19,7 +24,7 @@ svg.append("g")
     .translate(600, 250)
     .attr("id", "plot")
 
-function state_linear(){
+function stage_linear(){
     var x = d3.scale.linear()
             .domain([-5, 5])
             .range([-200, 200])
@@ -32,21 +37,27 @@ function state_linear(){
     var plot = svg.select("#plot");
     var layer1 = plot.append("g");
     var layer2 = plot.append("g");
-    var layer3 = plot.append("g");
-    layer2.append("line")
+    layer1.append("line")
         .attr({x1: x.range()[0], x2: x.range()[1], y1: 0, y2: 0})
-    layer2.append("line")
+    layer1.append("line")
         .attr({x1: x(0), x2: x(0), y1: 10, y2: -10})
-    // TODO: Use these layers to put the vertical lines under the black circles
 
-
-    // TODO: refactor this into smaller functions
     function render(initialRender){
-        var symbols = d3.select("#symbols").selectAll("text")
+        freeze();
+        circlesX(layer2, 0);
+        lines(layer1, 1);
+        circlesA(layer2, 1);
+        dragging(layer2, 2, initialRender);
+        symbols(svg.select("#symbols"), 3);
+        d3.timer(function(){unfreeze(); return true;}, 4*transDur);
+    }
+
+    var symbols = function(g, order){
+        var symbols = g.selectAll("text")
             .data(["a = mx + b", "a = " + m.toFixed(2) + "x + " + b.toFixed(2)])
         symbols.enter().append("text")
             .style("opacity", 0)
-          .transition().duration(500).delay(transDur*3)
+          .transition().duration(500).delay(transDur*order)
             .style("opacity", 1)
         symbols.exit()
           .transition().duration(500)
@@ -54,111 +65,113 @@ function state_linear(){
             .remove();
         symbols.text(function(d){return d})
             .translate(function(d,i){return [0, 30*i]})
+    }
 
-
-        var circles = plot.selectAll("g.datapoint")
+    var lines = function(g, order){
+        var lines = g.selectAll("line.a")
             .data(data)
-        circles.exit().transition().attr("r", 0).remove();
-
-        renderCircleX = function(sel){
-            sel.attr("r", 4)
-               .attr("cx", function(d){ return x(d)})
-        }
-        renderLineA = function(sel){
-            sel.attr("y2", function(d){ return -x(f(d))})
-        }
-        renderCircleA = function(sel){
-            sel.attr("r", function(d){return d ? 4 : 3})
-               .attr("cy", function(d){ return -x(f(d))})
-        }
-
-        // update selection only - must be called before appending to enter
-        circles.select("circle.x").call(renderCircleX)
-        circles.select("line.a").call(renderLineA)
-        circles.select("circle.a").call(renderCircleA)
-
-        var entering = circles.enter().append("g").attr("class", "datapoint")
-
-        entering.append("circle")
-            .attr("class", "x")
-            .attr("r", 0)
-            .attr("cx", x(0))
-            .attr("cy", 0)
-          .transition().duration(transDur)
-            .call(renderCircleX)
-
-        entering.append("line")
+        lines.exit().transition().style("opacity", 0).remove();
+        lines.attr("y2", function(d){ return -x(f(d))})
+        lines.enter().append("line")
             .attr("class", "a")
             .attr("x1", function(d){ return x(d)})
             .attr("x2", function(d){ return x(d)})
             .attr({y1: 0, y2: 0})
           .transition().delay(transDur).duration(transDur)
-            .call(renderLineA)
+            .attr("y2", function(d){ return -x(f(d))})
+    }
 
-        entering.append("circle")
-            .attr("r", 0)
-            .attr("cx", function(d){ return x(d)})
-            .attr("cy", 0)
-            .attr("class", "a")
-          .transition().delay(transDur).duration(transDur)
-            .call(renderCircleA)
+    var makeCircles = function(className, initialize, finalize){
+        return function(g, order){
+            var circles = g.selectAll("circle."+className)
+                .data(data)
+            circles.call(finalize);
+            circles.exit().transition().attr("r", 0).remove();
+            circles.enter().append("circle")
+                .attr("class", className)
+                .call(initialize)
+              .transition().duration(transDur).delay(transDur*order)
+                .call(finalize)
+        }
+    }
 
+    var circlesX = makeCircles("x",
+        function(sel){sel.attr("r", 0).attr("cx", x(0)).attr("cy", 0)},
+        function(sel){sel.attr("r", 4).attr("cx", function(d){ return x(d)})}
+    )
+
+    var circlesA = makeCircles("a",
+        function(sel){
+            sel.attr("r", 0).attr("cy", 0).attr("cx", function(d){ return x(d)})
+        },
+        function(sel){
+            sel.attr("r", 3)
+                .attr("cy", function(d){ return -x(f(d))})
+        }
+    )
+
+    var dragging = function(g, order, initialRender){
         var dx = x.range()[0]
         var dy = f(x(-3))
         var len = Math.sqrt(dx*dx + dy*dy)
         var rot = Math.atan(m) * -180 / Math.PI;
         if (initialRender){
-            var dragRot = d3.behavior.drag()
-                .on("drag", function(evt){
-                    if (d3.event.x == 0){
-                        m = 9999.99;
-                    }else{
-                        m = (-d3.event.y-x(b))/d3.event.x;
-                    }
-                    render();
-                })
-            var dragHandle = plot.append("g")
-                .attr("id", "draggerRot")
-                .attr("class", "dragger")
-                .attr("transform", "rotate("+rot+") translate(0,"+-x(b)+")")
-                .call(dragRot)
-            dragHandle.append("line")
-                .attr({x1: -len, x2: -len, y1: 0, y2: 0})
-                .attr("class", "a")
-              .transition().duration(transDur).delay(2*transDur)
-                .attr({x2: len})
-            dragHandle.append("line")
-                .attr("class", "cover")
-                .attr({x1: -len, x2: len, y1: 0, y2: 0})
-
-            var dragVert = d3.behavior.drag()
-                .on("drag", function(evt){
-                    b = -x.invert(d3.event.y)
-                    render();
-                })
-            var side = (x(1)-x(0))*0.8
-            plot.append("rect")
-                .attr("id", "draggerVert")
-                .attr("class", "cover dragger")
-                .attr("transform", "translate(0,"+-x(b)+")")
-                .attr("x", -side)
-                .attr("y", -side)
-                .attr("width", 2*side)
-                .attr("height", 2*side)
-                .call(dragVert)
+            dragRot(g, order, len);
+            dragVert(g, order);
         }else{
-            d3.select("#draggerRot")
+            g.select("#draggerRot")
                 .attr("transform", "rotate("+rot+",0,"+-x(b)+") translate(0,"+-x(b)+")")
               .select("line")
                 .attr({x1: -len, x2: len, y1: 0, y2: 0})
-            d3.select("#draggerVert")
+            g.select("#draggerVert")
                 .attr("transform", "translate(0,"+-x(b)+")")
         }
     }
 
-    freeze();
-    d3.timer(function(){unfreeze(); return true;}, 4*transDur);
+    var dragRot = function(g, order, len){
+        var drag = d3.behavior.drag()
+            .on("drag", function(evt){
+                if (d3.event.x === 0){
+                    m = 9999.99;
+                }else{
+                    m = (-d3.event.y-x(b))/d3.event.x;
+                }
+                render();
+            })
+        var dragHandle = g.append("g")
+            .attr("id", "draggerRot")
+            .attr("class", "dragger")
+            .attr("transform", "rotate(-45) translate(0,"+-x(b)+")")
+            .call(drag)
+        dragHandle.append("line")
+            .attr({x1: -len, x2: -len, y1: 0, y2: 0})
+            .attr("class", "a")
+          .transition().duration(transDur).delay(order*transDur)
+            .attr({x2: len})
+        dragHandle.append("line")
+            .attr("class", "cover")
+            .attr({x1: -len, x2: len, y1: 0, y2: 0})
+    }
+
+    var dragVert = function(g, order){
+        var drag = d3.behavior.drag()
+        .on("drag", function(evt){
+            b = -x.invert(d3.event.y)
+            render();
+        })
+        var side = (x(1)-x(0))*0.8
+        g.append("rect")
+            .attr("id", "draggerVert")
+            .attr("class", "cover dragger")
+            .attr("transform", "translate(0,"+-x(b)+")")
+            .attr("x", -side)
+            .attr("y", -side)
+            .attr("width", 2*side)
+            .attr("height", 2*side)
+            .call(drag)
+    }
+
     render(1, 0, true);
 }
 
-state_linear();
+stage_linear();
