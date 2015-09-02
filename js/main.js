79259b25c8803743f9089d257d0569336120702d
Max Goldstein <maxgoldstein1@gmail.com>
2015-09-02 16:37:31 -0400

    Wrap main code in window.onload

--- js/main.js
+++ js/main.js
@@ -6,25 +6,27 @@ require('./innersvg')
 
 require('./headers')
 
-var stage_linear = require('./stage_linear')()
-var stage_vector_line = require('./stage_vector_line')()
-var stage_scalar_field = require('./stage_scalar_field')()
-var stage_vector_field = require('./stage_vector_field')()
-var stage_vector_field3 = require('./stage_vector_field3')()
+window.onload = function(){
+    var stage_linear = require('./stage_linear')()
+    var stage_vector_line = require('./stage_vector_line')()
+    var stage_scalar_field = require('./stage_scalar_field')()
+    var stage_vector_field = require('./stage_vector_field')()
+    var stage_vector_field3 = require('./stage_vector_field3')()
 
-var stage = function(sel, f){
-    var bb = document.querySelector(sel).getBoundingClientRect();
-    var trigger = bb.top + bb.height * 0.8;
-    d3.timer(function(){
-        if (window.scrollY + window.innerHeight > trigger){
-            f();
-            return true; // stop timer to avoid reanimating
-        }
-    })
+    var stage = function(sel, f){
+        var bb = document.querySelector(sel).getBoundingClientRect();
+        var trigger = bb.top + bb.height * 0.8;
+        d3.timer(function(){
+            if (window.scrollY + window.innerHeight > trigger){
+                f();
+                return true; // stop timer to avoid reanimating
+            }
+        })
+    }
+
+    stage("svg.first", stage_linear);
+    stage("svg.second", stage_vector_line);
+    stage("svg.third", stage_scalar_field);
+    stage("svg.fourth", stage_vector_field);
+    stage("svg.fifth", stage_vector_field3);
 }
-
-stage("svg.first", stage_linear);
-stage("svg.second", stage_vector_line);
-stage("svg.third", stage_scalar_field);
-stage("svg.fourth", stage_vector_field);
-stage("svg.fifth", stage_vector_field3);
