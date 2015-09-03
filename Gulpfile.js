3c51c1880c3a5ee8a56321720bfc4c08229ecfc2
Max Goldstein <maxgoldstein1@gmail.com>
2015-09-03 15:12:05 -0400

    Update package.json

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,8 +1,8 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var rename = require('gulp-rename')
 var browserify = require('browserify');
 var source = require('vinyl-source-stream')
-var rename = require('gulp-rename')
 
 gulp.task('sass', function() {
     gulp.src('sass/**/*.scss')
